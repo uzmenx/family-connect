6f2f0be54c54f3df1030d01b9043f2dc9a83517f
uzmenx <baknistan@gmail.com>
2026-02-21 00:21:06 +0500

    .hshshshshsh..

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.avlodona.app',
+  appName: 'avlodona',
+  webDir: 'dist'
+};
+
+export default config;
